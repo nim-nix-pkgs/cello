47cf1d53b5139580bccfe329b2d15516879a1d22
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:58 -0300

    chore: re-index ../nimpkgs/c/cello/0_1_5

--- 0_1_5/flake.nix
+++ 0_1_5/flake.nix
@@ -9,13 +9,14 @@
   
   inputs.src-cello-0_1_5.flake = false;
   inputs.src-cello-0_1_5.owner = "andreaferretti";
-  inputs.src-cello-0_1_5.ref   = "refs/tags/0.1.5";
+  inputs.src-cello-0_1_5.ref   = "0_1_5";
   inputs.src-cello-0_1_5.repo  = "cello";
   inputs.src-cello-0_1_5.type  = "github";
   
   inputs."spills".owner = "nim-nix-pkgs";
   inputs."spills".ref   = "master";
   inputs."spills".repo  = "spills";
+  inputs."spills".dir   = "0_1_3";
   inputs."spills".type  = "github";
   inputs."spills".inputs.nixpkgs.follows = "nixpkgs";
   inputs."spills".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
